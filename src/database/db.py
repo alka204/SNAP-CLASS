b464f12c8cfb5e6715302fab167edc134c95390b
Alka Kumari <alka.kumari3289@gmail.com>
2026-05-27 17:27:22 +0530

    fixed attendance timestamp

--- src/database/db.py
+++ src/database/db.py
@@ -49,14 +49,14 @@ def create_subject(subject_code, name, section, teacher_id):
     return response.data
 
 def get_teacher_subjects(teacher_id):
-    response = supabase.table('subjects').select("*, subject_students(count), attendance_logs(timestamp)").eq("teacher_id", teacher_id).execute()
+    response = supabase.table('subjects').select("*, subject_students(count), attendance_logs(timestampp)").eq("teacher_id", teacher_id).execute()
     subjects = response.data
 
 
     for sub in subjects:
         sub['total_students'] = sub.get("subject_students", [{}])[0].get('count', 0) if sub.get('subject_students') else 0
         attendance = sub.get('attendance_logs', [])
-        unique_sessions = len(set(log['timestamp'] for log in attendance))
+        unique_sessions = len(set(log['timestampp'] for log in attendance))
         sub['total_classes'] = unique_sessions
 
 
